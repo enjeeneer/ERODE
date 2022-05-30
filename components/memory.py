b5e4edcf7334fbf64ff53b14595ab65f7048e563
enjeeneer <scottjeen@gmail.com>
2022-05-30 16:58:25 +0100

    fix: updated sample() function to return trajectories as well as input/output pairs

--- components/memory.py
+++ components/memory.py
@@ -153,31 +153,67 @@ class ModelFreeMemory:
         self.previous[self.obs_dim:] = state
 
 class ErodeMemory:
-    def __init__(self, batch_size, hist_length, obs_dim, net_inp_dims):
+    def __init__(self, batch_size, hist_length, horizon, traj_samples, obs_dim, net_inp_dims):
         self.model_inputs = []
-        self.obs = []
         self.obs_next = []
         self.rewards = []
         self.batch_size = batch_size
+        self.horizon = horizon
+        self.traj_batches = traj_batches
         self.hist_length = hist_length
         self.obs_dim = obs_dim # obs_dim + time_dim
         self.net_inp_dims = net_inp_dims
         self.history = np.zeros(shape=(hist_length, self.net_inp_dims))
 
     def sample(self):
-        '''
-        Generates batches of training data for dynamical model from previously executed state actions and observations
-        :return: array of all stored state actions of shape (datapoints, act_dim+obs_dim)
-        :return: array of all stored observations of shape (datapoints, obs_dim)
-        :return: array of batch indices of shape (datapoints/batch_size, batch_size)
-        '''
-
+        """
+        Generates batches of training data for dynamical model and value/policy functions
+        :return inp_model: array of model inputs of shape (model_batches, batch_size, model_inp_dim)
+        :return obs_model: array of observations from model inputs (model_batches, batch_size, obs_dim)
+        :return inp_trajs: array of input trajectories of shape (traj_batches, horizon, model_inp_dim)
+        :return obs_trajs: array of observation trajectories of shape (traj_batches, horizon, obs_dim)
+        :return reward_trajs: array of reward trajectories of shape (traj_batches, horizon, 1)
+        """
         datapoints = len(self.model_inputs)
-        batch_start = np.arange(0, datapoints, self.batch_size)
-        indices = np.random.choice(datapoints, size=datapoints, replace=True)
-        batches = [indices[i:i + self.batch_size] for i in batch_start]
 
-        return np.array(self.model_inputs), np.array(self.observations), batches
+        # samples trajectories
+        inp_trajs = np.zeros(shape=(self.traj_batches, self.horizon, self.net_inp_dims))
+        obs_trajs = np.zeros(shape=(self.traj_batches, self.horizon, self.obs_dim))
+        reward_trajs = np.zeros(shape=(self.traj_batches, self.horizon, 1))
+
+        traj_batch_starts = np.random.choice(datapoints - self.horizon, size=self.traj_batches)
+        for i, idx in enumerate(traj_batch_starts):
+            traj_idxs = np.arange(idx, idx+self.horizon)
+
+            # index into memory
+            inps = self.model_inputs[traj_idxs]
+            obs = self.obs_next[traj_idxs]
+            rewards = self.rewards[traj_idxs]
+
+            # add to sample
+            inp_trajs[i, :, :] = inps
+            obs_trajs[i, :, :] = obs
+            reward_trajs[i, :, :] = rewards
+
+        # samples input/output pairs in batches for model
+        model_batches = round(datapoints / self.batch_size)
+        inp_model = np.zeros(shape=(model_batches, self.batch_size, self.net_inp_dims))
+        obs_model = np.zeros(shape=(model_batches, self.batch_size, self.obs_dim))
+
+        model_batch_starts = np.arange(0, datapoints, self.batch_size)
+        idxs = np.random.choice(datapoints, size=model_batches*self.batch_size, replace=True)
+        batches = [idxs[i:i + self.batch_size] for i in model_batch_starts]
+
+        for i, batch in batches:
+            # index into memory
+            inps = self.model_inputs[batch]
+            outs = self.obs[batch]
+
+            # add to sample
+            inp_model[i, :, :] = inps
+            obs_model[i, :, :] = outs
+
+        return inp_model, obs_model, inp_trajs, obs_trajs, reward_trajs
 
     def store(self, model_input, obs, obs_next, reward):
         '''

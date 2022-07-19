a0661e7d0bd02369d122f544cf76ce3977225fa9
enjeeneer <scottjeen@gmail.com>
2022-07-19 09:50:29 +0100

    fix: combined acts

--- agents/erode.py
+++ agents/erode.py
@@ -138,8 +138,8 @@ class Agent(Base):
 
             state_tile = pred_states.cpu().detach().numpy()
 
-        combined_acts = np.concatenate((stoch_acts, pi_acts), axis=1)  # [particles, popsize, horizon, act_dim]
-        assert combined_acts.shape == (self.cfg.particles, self.cfg.popsize, self.cfg.horizon, self.act_dim)
+        combined_acts = np.concatenate((stoch_acts, pi_acts), axis=1).squeeze(axis=0)  # [particles, popsize, horizon, act_dim]
+        assert combined_acts.shape == (self.cfg.popsize, self.cfg.horizon, self.act_dim)
 
         return trajs, combined_acts
 
@@ -184,7 +184,7 @@ class Agent(Base):
                 trajs, combined_acts = self.traj_sampler(obs, stoch_acts)
 
                 exp_rewards = self.estimate_value(trajs, combined_acts).cpu().detach().numpy()  # returns pi_actions appended to CEM actions
-                elites = combined_acts[np.argsort(exp_rewards) ][:int(self.cfg.elites * self.cfg.popsize)]
+                elites = combined_acts[np.argsort(exp_rewards)][:int(self.cfg.elites * self.cfg.popsize)]
 
                 new_mean = torch.mean(elites, axis=0)
                 new_var = torch.var(elites, axis=0)
